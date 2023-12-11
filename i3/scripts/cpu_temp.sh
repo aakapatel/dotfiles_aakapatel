0e0daa3a84b36d5af8a5f1647eb0d9c7ce510f4d
aakapatel <aakapatel@ubuntu-server>
2023-12-11 13:46:05 +0100

    new pc -- solarized osaka

--- i3/scripts/cpu_temp.sh
+++ i3/scripts/cpu_temp.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # Fetch CPU temperature using sensors command and extract the temperature value
-temp=$(sensors | awk '/Core 0/ {print $3}')
-
+# temp=$(sensors | awk '/Core 0/ {print $3}')
+temp=$(sensors | grep 'Package id' | awk '{print $4}')
 if [ -z "$temp" ]; then
-    echo "CPU Temp: N/A"
+	echo "CPU Temp: N/A"
 else
-    echo "CPU Temp: $temp"
+	echo "CPU Temp: $temp"
 fi
+#
+#
+
+#
+# # Convert millidegrees Celsius to degrees Celsius
+# TEMP_C=$(echo "scale=2; $TEMP / 1000" | bc)
+#
+# echo "$TEMP_C"
