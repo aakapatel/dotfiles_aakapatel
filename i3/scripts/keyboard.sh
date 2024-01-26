366f06b506de3fdb2841519b1b7935b481495009
aakapatel <patelakash94293@gmail.com>
2024-01-26 10:39:45 +0100

    u22 on p14s config

--- i3/scripts/keyboard.sh
+++ i3/scripts/keyboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 setxkbmap -layout us
-~/./tmp/xborder/xborders --border-red 25 --border-green 150 --border-blue 25 --border-width 2 --border-radius 20
+# ~/./tmp/xborder/xborders --border-red 25 --border-green 150 --border-blue 25 --border-width 2 --border-radius 20
