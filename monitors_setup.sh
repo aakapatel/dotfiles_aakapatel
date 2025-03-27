16f49f858fd764fcf69f8ba1283dfee11ef59565
aakapatel <patelakash94293@gmail.com>
2025-03-27 21:33:22 +0100

    removed monitors config file

--- monitors_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Detect external monitor (assuming "HDMI-A-1" is your external at home)
-if hyprctl monitors | grep -q "DP-3"; then
-    ln -sf ~/.config/hypr/monitors_home.conf ~/.config/hypr/monitors.conf
-else
-    ln -sf ~/.config/hypr/monitors.conf ~/.config/hypr/monitors.conf
-fi
